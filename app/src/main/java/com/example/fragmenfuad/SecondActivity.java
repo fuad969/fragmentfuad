8c8329d4984c8ea222a739d9d83c2babb2d82e6b
fuad969 <isfuadii@gmail.com>
2021-04-18 17:47:03 +0700

    third commit

--- app/src/main/java/com/example/fragmenfuad/SecondActivity.java
+++ app/src/main/java/com/example/fragmenfuad/SecondActivity.java
@@ -1,6 +1,25 @@
 package com.example.fragmenfuad;
 
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Log.v("CREATING", "SECOND ACTIVITY");
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            finish();
+            return;
+        }
+        if (savedInstanceState == null) {
+            SecondFragment details = new SecondFragment();
+            getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+        }
+    }
 }
